51b16fff455bb1cda20ec45d5f3a3624e7972812
Arge2004 <argenismedina0424@gmail.com>
2025-08-02 22:33:35 -0500

    [FEAT] Visual changes

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,10 @@
+import project1 from "../assets/projects/project1.png?url";
+
 export const projects = [
   {
     "title": "Github Profile Visualizer",
     "description": "A simple web application to visualize GitHub profiles.",
-    "imageUrl": "/projects/project1.png",
+    "imageUrl": project1,
     "techStack": ["React", "TypeScript", "TailwindCSS"],
     "demoUrl": "https://github-profile-visualizer-eosin.vercel.app/",
     "codeUrl": "https://github.com/Arge2004/Github-Profile-Visualizer"
@@ -10,7 +12,7 @@ export const projects = [
   {
     "title": "Project One",
     "description": "This is a description of project one.",
-    "imageUrl": "/projects/project1.png",
+    "imageUrl": project1,
     "techStack": ["React", "TypeScript", "CSS3"],
     "demoUrl": "https://demo.projectone.com",
     "codeUrl": "https://github.com/user/project-one"
@@ -18,7 +20,7 @@ export const projects = [
   {
     "title": "Project One",
     "description": "This is a description of project one.",
-    "imageUrl": "/projects/project1.png",
+    "imageUrl": project1,
     "techStack": ["React", "TypeScript", "CSS3"],
     "demoUrl": "https://demo.projectone.com",
     "codeUrl": "https://github.com/user/project-one"
